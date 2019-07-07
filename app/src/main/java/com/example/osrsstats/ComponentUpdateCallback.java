f24155475e81fae41b7433553f37888383c15783
EvaKozakova26 <evkozakova@seznam.cz>
2019-07-07 18:47:21 +0200

    add comments

--- app/src/main/java/com/example/osrsstats/ComponentUpdateCallback.java
+++ app/src/main/java/com/example/osrsstats/ComponentUpdateCallback.java
@@ -1,6 +1,13 @@
 package com.example.osrsstats;
 
+/**
+ * Sets values of component in Activity from other class
+ */
 public interface ComponentUpdateCallback {
 
+   /**
+    * Updates TextView
+    * @param text text to update
+    */
    void updateTextView(String text);
 }
